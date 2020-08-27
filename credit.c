844ebf2c56d396648ac6355dc809270efd8693da
benson00077 <benson00077@users.noreply.github.com>
2020-08-27 19:00:01 +0000

    automated commit by check50 [check50=True]

--- credit.c
+++ credit.c
@@ -1,86 +1,115 @@
 #include <stdio.h>
 #include <cs50.h>
 
+long number(void);
+long calsd(long cardnumber);
+long calfd(long cardnumber);
+long length(long cardnumber);
+void checksum(long n, int sum, int len);
+
 int main(void)
 {
-    //////// get cardnumber from prompt ////////
-    long cardnumber;
-    do
-    {
-        cardnumber = get_long("Number: ");
-    }
-    while (cardnumber < 0);
+    long n = number();
+   
+    long cal2 = calsd(n);
+    long cal1 = calfd(n);
+    long sum = cal2 + cal1;
     
-    /////// calculate checksum ///////
-    long checksum = 0;
-    int length = 0;
-    int first2digit = 0;
-    long cnd = 0;  /// card number second-to-last digit 
-    long cndf = 0; /// card number first-to-last digit 
+    long len = length(n);
+   
+    checksum(n, sum, len);
     
-    for (long d = 100; d < cardnumber*10; d *= 100) //iterate start from the second-to-last digit
-    {
-        cnd = ((cardnumber%d - cardnumber%(d/10)) / (d/10))*2;
-        length += 1;
-        /// printf("Digit: %li\n", cnd);
-        if (cnd >= 10)
-        {
-            cnd = (cnd%100 - cnd%10) / 10 + (cnd%10);
-            checksum += cnd;
-        }
-        else if (cnd < 10 )
-        {
-            checksum += cnd;
-        }
-        
-    }
-    /// printf("%li\n\n", checksum);
-    
-    for (long df = 10; df < cardnumber*10; df *= 100) //start fro the first-to-last digit
-    {
-        length += 1;
-        cndf = ((cardnumber%df - cardnumber%(df/10)) / (df/10));
-        /// printf("cndf= %li\n", cndf);
-        checksum += cndf;
-        /// printf("%li\n", checksum);
-    }
-    ///printf("Length: %i\n", length); ////// get lentgh plus one after evert loop
- 
-    ////// Check for card lentgh to catagorize wich card belong and validate accordingly ///////
-    if (checksum % 10 == 0) /// valid number
-    {
-        if (length % 2 == 0)
-        {
-            first2digit += cnd/2 * 10 + cndf;
-        }
-        else if (length % 2 == 1)
-        {
-            first2digit += cndf * 10 + cnd;
-        }
-        
-        ///printf ("%i\n", first2digit);
-        
-        if (length == 15 && (first2digit == 34 || first2digit == 37))
-        {
-            printf("AMEX\n");
-        }
-        else if (length == 16 && 51 <= first2digit && first2digit <= 55)
-        {
-            printf("MASTERCARD\n");
-        }
-        else if ((length == 13 || length == 16) && (first2digit - (first2digit % 10))/10  == 4)
-        {
-            printf("VISA\n");
-        }
-    }
-    else
-    {
-        printf("INVALID\n");
-    }
 }
 
 
-void checksum(void)
+/// get cardnumber from prompt 
+long number(void)
 {
+    long n;
+    do
+    {
+        n = get_long("Number: ");
+    }
+    while (n < 1);
+    return n;
+}
+
+/// even digits sum
+long calsd(long cardnumber)
+{
+    long sd; ///the second-to-last digit
+    long sdsum = 0; /// sum of the digits
     
+    for (long d = 100; d < cardnumber*10; d *= 100) ///Plus every other digits by2, starting from the second-to-last digit
+    {
+        sd = (cardnumber%d - cardnumber%(d/10)) / (d/10);
+        sd *= 2;
+        if (sd >= 10)
+        {
+            sd = (sd%100 - sd%10) / 10 + (sd%10);
+        }
+        sdsum += sd;
+    }
+    return sdsum;
+}
+
+/// odd digits sum
+long calfd(long cardnumber)
+{
+    long fd; ///the first-to-last digit
+    long fdsum = 0; /// sum of the digits  
+    
+    for (long d = 10; d < cardnumber*10; d *= 100) ///Plus every other digits by2, starting from the first-to-last digit
+    {
+        fd = ((cardnumber%d - cardnumber%(d/10)) / (d/10));
+        fdsum += fd;
+    }
+    return fdsum;
+}
+
+
+/// check length of the cardnumber, unconsidet those start with 0, 00, 000 or so 
+long length(long cardnumber)
+{
+    long len = 0;
+    for (long d = 1; d < cardnumber; d *= 10)
+    {
+        len += 1;
+    }
+    return len;
+}
+
+
+
+/// checksum
+void checksum(long n, int sum, int len)
+{
+    long f2d; /// first 2 digit
+    long pwr = 1;
+    for (int i=0; i<len-2; i++) /// 10 to the power of len-2
+    {
+        pwr *= 10;
+    }
+    f2d = (n - (n % pwr))/pwr; /// get first 2 digit of cardnumber
+    
+
+    
+    
+    if (sum % 10 == 0)
+    {
+        if ((f2d == 34 || f2d == 37) && len == 15)
+        {
+            printf("AMEX\n");
+        }
+        else if (51 <= f2d && f2d <= 55 && len == 16)
+        {
+            printf("MASTERCARD\n");
+        }
+        else if  ((f2d - (f2d%10)) / 10 == 4 && (len == 13 || len == 16))
+        {
+            printf("VISA\n");
+        }
+        else printf("INVALID\n");
+    }
+    else printf("INVALID\n");
 }
