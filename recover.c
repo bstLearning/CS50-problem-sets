a644e3143522e8c4af74b5625446f2096a63a74b
benson00077 <benson00077@users.noreply.github.com>
2020-10-02 05:11:39 +0000

    automated commit by check50 [check50=True]

--- recover.c
+++ recover.c
@@ -1,5 +1,8 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdint.h>
+
+typedef uint8_t BYTE;
 
 int main(int argc, char *argv[])
 {
@@ -17,29 +20,51 @@ int main(int argc, char *argv[])
     
     // counter of i'th JPEG, index of i'th byte that srart the first JPEG
     int counter = 0;
-    int starter = 0;
+    int starter = 0;    
+    
+    FILE *img = NULL;
     
     
     // Read 512 bytes into a buffer 
     // char *buffer = malloc(sizeof(char)*512);
-    unsigned char *buffer = malloc(sizeof(char) * 512);
+    BYTE *buffer = malloc(sizeof(BYTE) * 512);   //// >>>>>>>>>>>>>>>>>>>>>>>><<<<<<<<<<<<<<<<<<<<    or typedef uint8_t BYTE;
 
-    while(fread(buffer, sizeof(char), 512, f) == 512)
+    while(fread(buffer, sizeof(BYTE), 512, f) == 512)
     {
         if (buffer[0] == 0xff && buffer[1] == 0xd8 && buffer[2] == 0xff && (buffer[3] & 0xf0) == 0xe0) // bitwise arithmetic
             {
                 // Makeing a new JPEG, filenames: ###.jpg
                 char filename[8];
-                sprintf(filename, "%03i.jpg", counter);  // print not to a terminal, but a string "filename". print 002.jpeg
-            
-                // open a new file (img) and write in the first JPEG
-                FILE *img = fopen(filename, "w");
-                fwrite(buffer, sizeof(char), 512, img);
-                fclose(img);
+                sprintf(filename, "%03i.jpg", counter);  // print not to a terminal, but a string "filename". print 002.jpg/NULL
                 
-                counter ++;
+                // if first JPEG
+                if (starter == 0)
+                {
+                    // open a new file (img) and write in the first JPEG
+                    img = fopen(filename, "w");
+                    if (!img)
+                         {return 3;}
+                    fwrite(buffer, sizeof(BYTE), 512, img);
+                    starter ++;
+                    counter ++;
+                }
+                // if not first JPEG
+                else 
+                {
+                    fclose(img);
+                    img = fopen(filename, "w");
+                    if (!img)
+                        {return 3;}
+                    fwrite(buffer, sizeof(BYTE), 512, img);
+                    counter ++;
+                }
             }
+        else if (starter != 0)
+        {
+            fwrite(buffer, sizeof(BYTE), 512, img);
+        }
     }
+    fclose(img);
     free(buffer);
     fclose(f);
 }
