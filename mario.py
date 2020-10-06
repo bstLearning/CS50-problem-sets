6cd75e7c68f58bb000cb592271eb1e6da3d1c639
benson00077 <benson00077@users.noreply.github.com>
2020-10-06 16:53:26 +0000

    automated commit by submit50

---
+++ mario.py
@@ -0,0 +1,12 @@
+import cs50 
+
+
+while True:
+    h = cs50.get_int("Height: ")
+    if 0 < h < 9:
+        break
+
+for i in range (1, h+1):
+    spacenumber = h+1-i
+    print(" " * spacenumber + "#" * i + "  " + "#" * i)
+
