4596dcaf8904093ffcc342bcaaddbfb708b11ff9
benson00077 <benson00077@users.noreply.github.com>
2020-10-07 07:26:49 +0000

    automated commit by submit50

--- mario.py
+++ mario.py
@@ -6,7 +6,10 @@ while True:
     if 0 < h < 9:
         break
 
-for i in range (1, h+1):
+for i in range(1, h+1):
     spacenumber = h+1-i
     print(" " * spacenumber + "#" * i + "  " + "#" * i)
 
+## feedback from sytle50
+## Looks good!
+## But consider adding more comments!
