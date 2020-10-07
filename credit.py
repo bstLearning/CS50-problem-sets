c65f8bd52e7d0919b02ecd237c945b4ce0f87019
benson00077 <benson00077@users.noreply.github.com>
2020-10-07 07:44:50 +0000

    automated commit by submit50

--- credit.py
+++ credit.py
@@ -14,7 +14,7 @@ def number():
             break
     return n
     
-#even digits sum (count from second-to-last digit)
+# even digits sum (count from second-to-last digit)
 def calsd(n):
     sdsum = 0
     for i in range(1, int(len(str(n))/2) + 1):
@@ -27,7 +27,7 @@ def calsd(n):
     return sdsum
         
     
-#odd digits sum (count from first-to-last digit)
+# odd digits sum (count from first-to-last digit)
 def calfd(n):
     fdsum = 0
     for i in range(int(len(str((n)))/2 + 1)):
@@ -38,14 +38,14 @@ def calfd(n):
 
 # checksum
 def checksum(n, sum, len):
-    f2d = (n - n % (10 ** (len-2))) / (10 ** (len-2)) # first 2 digit of cardnumber
+    f2d = (n - n % (10 ** (len-2))) / (10 ** (len-2))  # first 2 digit of cardnumber
     
     if int(sum) % 10 == 0:
         if (f2d == 34 or f2d == 37 ) and len == 15:
             print("AMEX")
         elif 51 <= f2d <= 55 and len == 16:
             print("MASTERCARD")
-        elif (f2d - f2d%10) / 10 == 4 and (len == 13 or len == 16):
+        elif (f2d - f2d % 10) / 10 == 4 and (len == 13 or len == 16):
             print("VISA")
         else:
             print("INVALID")
