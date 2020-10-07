f537af25a0ae288eebfd552a98593057be0db6ac
benson00077 <benson00077@users.noreply.github.com>
2020-10-07 07:31:16 +0000

    automated commit by submit50

--- mario.py
+++ mario.py
@@ -1,15 +1,23 @@
-import cs50 
+import cs50
 
 
-while True:
-    h = cs50.get_int("Height: ")
-    if 0 < h < 9:
-        break
+def main():
+    pyramid()
+    
+    
+def pyramid():
+    while True:
+        h = cs50.get_int("Height: ")
+        if 0 < h < 9:
+            break
+    
+    for i in range(1, h+1):
+        spacenumber = h+1-i
+        print(" " * spacenumber + "#" * i + "  " + "#" * i)
 
-for i in range(1, h+1):
-    spacenumber = h+1-i
-    print(" " * spacenumber + "#" * i + "  " + "#" * i)
 
-## feedback from sytle50
-## Looks good!
-## But consider adding more comments!
+main()
+
+# feedback from sytle50
+# Looks good!
+# But consider adding more comments!
