808a0318957a0c98234deb3e4344d58c53277a02
benson00077 <benson00077@users.noreply.github.com>
2020-10-12 07:41:54 +0000

    automated commit by submit50

--- 11.sql
+++ 11.sql
@@ -1,9 +1,10 @@
-SELECT title 
+SELECT title
 FROM movies
-WHERE id IN (
+	JOIN ratings ON movies.id = ratings.movie_id
+WHERE movies.id IN (
     SELECT ratings.movie_id 
     FROM ratings 
         JOIN stars ON ratings.movie_id = stars.movie_id 
         WHERE stars.person_id = (
-        select id from people where name = "Chadwick Boseman")
-    ORDER BY ratings.rating DESC LIMIT 5);
+        select id from people where name = "Chadwick Boseman"))
+ORDER BY ratings.rating DESC LIMIT 5;
--- 9.sql
+++ 9.sql
@@ -3,4 +3,5 @@ FROM people
 WHERE id IN (
     SELECT person_id FROM 
     stars JOIN movies ON stars.movie_id = movies.id
-    WHERE movies.year = 2004);
+    WHERE movies.year = 2004)
+ORDER BY people.birth;
